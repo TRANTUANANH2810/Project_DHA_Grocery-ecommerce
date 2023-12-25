7314c3a87d7be68f8235a3af11d09a98a37dd4fa
tuvd <vodinhtu28@gmail.com>
2023-12-25 21:13:04 +0700

    send mail for register seller

---
+++ app/Mail/MailInit.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MailInit extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $mailData;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Mail Init',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return (new Content(
+            view: 'mails.mailInit',
+        ))->with([
+            'mailData' => $this->mailData,
+        ]);
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
